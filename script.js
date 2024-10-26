e50f9634f086db5d31f7af890a2f452c8586416b
Jon Wood <jonwood789@gmail.com>
2024-10-26 19:06:10 -0400

    On branch update-overhaul Changes to be committed:     modified:   index.html     modified:   script.js     modified:   style.css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -9,11 +9,11 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if(top>=offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
+                document.querySelector('header nav a [href*='+id+' ]').classList.add('active')
             })
         }
-    });
+    })
 }
